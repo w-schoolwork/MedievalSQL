83e7d1615d928a82842829a60a7e0ac98646c707
Willow <willow.ch@protonmail.com>
2023-09-10 18:32:50 +0000

    add brand-neutral falling block game

--- src/www/index.rs
+++ src/www/index.rs
@@ -3,10 +3,15 @@ use rocket_dyn_templates::{context, Template};
 
 #[must_use]
 pub fn routes() -> Vec<Route> {
-	routes![index]
+	routes![index, practice]
 }
 
 #[get("/")]
 fn index() -> Template {
 	Template::render("index", context! {})
+}
+
+#[get("/practice")]
+fn practice() -> Template {
+	Template::render("game", context! {})
 }
---
+++ static/game.js
@@ -0,0 +1,31 @@
+$('.game').blockrain({
+	showFieldOnStart: true,
+	playText: 'Welcome to brand-neutral block game',
+	restartButtonText: 'Try again',
+	theme: {
+		background: '#040304',
+		backgroundGrid: '#000',
+		complexBlocks: {
+			line: ['/static/blockrain/assets/blocks/custom/line.png', '/static/blockrain/assets/blocks/custom/line.png'],
+			square: '/static/blockrain/assets/blocks/custom/square.png',
+			arrow: '/static/blockrain/assets/blocks/custom/arrow.png',
+			rightHook: '/static/blockrain/assets/blocks/custom/rightHook.png',
+			leftHook: '/static/blockrain/assets/blocks/custom/leftHook.png',
+			rightZag: '/static/blockrain/assets/blocks/custom/rightZag.png',
+			leftZag: '/static/blockrain/assets/blocks/custom/leftZag.png'
+		}
+	},
+
+	onStart: () => {
+		document.querySelector('#ready').disabled = true;
+	},
+
+	onGameOver: (score) => {
+		document.querySelector('#score').value = score;
+		document.querySelector('#ready').disabled = false;
+	},
+
+	onLine: (lines, scoreIncrement, score) => {
+		document.querySelector('#score').value = score;
+	}
+})
