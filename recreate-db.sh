474fc054a9a8109616e0d09cef25ff14f339abe4
Willow Carlson-Huber <git@wolo.dev>
2023-09-14 15:29:57 +0000

    add recreate-db

---
+++ recreate-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$DATABASE_URL" ]; then
+	>&2 echo "You need to set the \"DATABASE_URL\" environment variable so we know what to log into."
+	exit 1
+fi
+
+cargo sqlx database drop
+cargo sqlx database create
+cargo sqlx migrate run
