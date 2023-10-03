d594c05267e0819ad7ee625727ff4c6b03c23919
Willow Carlson-Huber <git@wolo.dev>
2023-10-03 11:30:49 -0500

    reduce dependence on vscode devcontainers a little

---
+++ prepare-outside.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker compose -f .devcontainer/docker-compose.yml up -d --build
+docker compose -f .devcontainer/docker-compose.yml exec -w/ws -u 0 app chown -R $(id -u) /ws
+docker compose -f .devcontainer/docker-compose.yml exec -w/ws -u $(id -u) app bash -c 'cargo sqlx migrate run && cargo sqlx prepare'
+docker compose -f .devcontainer/docker-compose.yml down
---
+++ recreate-outside.bat
@@ -0,0 +1,5 @@
+:: I have no idea if this will work
+docker compose -f .devcontainer/docker-compose.yml up -d --build
+docker compose -f .devcontainer/docker-compose.yml exec -w/ws -u 0 app chown -R $(id -u) /ws
+docker compose -f .devcontainer/docker-compose.yml exec -w/ws -u $(id -u) app bash ./recreate-db.sh
+docker compose -f .devcontainer/docker-compose.yml down
