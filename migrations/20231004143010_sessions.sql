477c41d1202c506cb0e96d71546611822ecd4810
Willow Carlson-Huber <git@wolo.dev>
2023-10-17 14:41:06 -0500

    whoops that wasn't semantically correct

--- migrations/20231004143010_sessions.sql
+++ migrations/20231004143010_sessions.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
 
 CREATE TABLE session (
-  user_id UUID PRIMARY KEY,
+  user_id UUID NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(user_id),
-  secret TEXT NOT NULL
+  secret TEXT NOT NULL UNIQUE
 );
